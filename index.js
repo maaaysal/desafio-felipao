e90df882aa56d7e6b1b5e712e45cbe551dfa57ea
maaaysal <maysaalucia@gmail.com>
2024-01-31 18:54:25 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-var nomeHeroi = prompt("Digite o nome do herói:");
+
+       let nomeHeroina;
+
+       nomeHeroina = prompt ('Digite seu nome de heroína: ');
+
+        let xpHeroi;
+
+        xpHeroi = prompt ('Digite a quantidade XP que você possui. ')
+
+         
+        if (xpHeroi < 1000) {
+            nivel = "Ferro";
+        } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+            nivel = "Bronze";
+        } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+            nivel = "Prata";
+        } else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+            nivel = "Ouro";
+        } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+            nivel = "Platina";
+        } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+            nivel = "Ascendente";
+        } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+            nivel = "Imortal";
+        } else {
+            nivel = "Radiante";
+        }
+        
+        console.log("O Herói de nome " + nomeHeroina + " está no nível de " + nivel);
+
